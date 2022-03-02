a0b0847279770df18ed3e0984763835bceb2557f
furkandadali <alifdadali@gmail.com>
2022-03-02 16:34:33 +0300

    project created. and tested.

---
+++ Security.Enigma/Decryption.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+
+namespace Security.Enigma
+{
+    public class Decryption
+    {
+        public static string DecryptTextInput(string encrypt, string haskKey)
+        {
+            using (TripleDES tripleDesService = TripleDES.Create())
+            {
+                using (MD5 hashMD5Service = MD5.Create())
+                {
+                    byte[] byteHash = hashMD5Service.ComputeHash(Encoding.UTF8.GetBytes(haskKey));
+                    tripleDesService.Key = byteHash;
+                    tripleDesService.Mode = CipherMode.ECB;
+                    byte[] data = Convert.FromBase64String(encrypt);
+                    return Encoding.UTF8.GetString(tripleDesService.CreateDecryptor().TransformFinalBlock(data, 0, data.Length));
+                }
+            }
+        }
+    }
+}
---
+++ Security.Enigma/Encryption.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+
+namespace Security.Enigma
+{
+    public class Encryptions
+    {
+        public static string EncryptTextInput(string source,string haskKey)
+        {
+            using (TripleDES tripleDesService = TripleDES.Create())
+            {
+                using (MD5 hashMD5Service = MD5.Create())
+                {
+                    byte[] byteHash = hashMD5Service.ComputeHash(Encoding.UTF8.GetBytes(haskKey));
+                    tripleDesService.Key = byteHash;
+                    tripleDesService.Mode = CipherMode.ECB;
+                    byte[] data = Encoding.UTF8.GetBytes(source);
+                    return Convert.ToBase64String(tripleDesService.CreateEncryptor().TransformFinalBlock(data, 0, data.Length));
+                }
+            }
+        }
+    }
+}
